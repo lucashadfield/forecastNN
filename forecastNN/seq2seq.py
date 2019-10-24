6e54e96cdcdbbc76519d9d861ec84d55596e8943
Lucas <lucas.hadfield@gmail.com>
2019-10-23 17:07:06 -0700

    Converted _build_model into a static method. Fixed a bug in the ordering of arguments in fit_df

--- forecastNN/seq2seq.py
+++ forecastNN/seq2seq.py
@@ -5,24 +5,19 @@ from tensorflow import keras
 
 class Seq2SeqForecaster(keras.models.Sequential):
     def __init__(self, pred_steps: int, nn_width: int = 20, nn_depth: int = 2):
+        super().__init__(self._build_model(pred_steps, nn_width, nn_depth))
         self.pred_steps = pred_steps
-        self.nn_width = nn_width
-        self.nn_depth = nn_depth
         self.fit_history = []
-        super().__init__(self._build_model())
 
-    def _build_model(self):
+    @staticmethod
+    def _build_model(pred_steps, nn_width, nn_depth):
         return (
-            [
-                keras.layers.LSTM(
-                    self.nn_width, return_sequences=True, input_shape=[None, 1]
-                )
-            ]
+            [keras.layers.LSTM(nn_width, return_sequences=True, input_shape=[None, 1])]
             + [
-                keras.layers.LSTM(self.nn_width, return_sequences=True)
-                for _ in range(self.nn_depth - 1)
+                keras.layers.LSTM(nn_width, return_sequences=True)
+                for _ in range(nn_depth - 1)
             ]
-            + [keras.layers.TimeDistributed(keras.layers.Dense(self.pred_steps))]
+            + [keras.layers.TimeDistributed(keras.layers.Dense(pred_steps))]
         )
 
     def compile(self, loss='mse', optimizer='adam', **kwargs):
@@ -36,23 +31,19 @@ class Seq2SeqForecaster(keras.models.Sequential):
         series = df.values.reshape(*df.shape, 1)
 
         X_train = series[:train_size, :n_steps]
-        X_valid = series[train_size:, :n_steps]
+        X_val = series[train_size:, :n_steps]
 
         Y = np.empty((n_series, n_steps, self.pred_steps))
         for step_ahead in range(1, 10 + 1):
             Y[..., step_ahead - 1] = series[..., step_ahead : step_ahead + n_steps, 0]
 
         Y_train = Y[:train_size]
-        Y_valid = Y[train_size:]
+        Y_val = Y[train_size:]
 
-        return X_train, X_valid, Y_train, Y_valid
+        return X_train, Y_train, X_val, Y_val
 
     def fit_df(
-        self,
-        df: pd.DataFrame,
-        epochs: int = 20,
-        validation_split: float = 0.2,
-        **kwargs
+        self, df: pd.DataFrame, epochs: int, validation_split: float = 0.2, **kwargs
     ):
         X_train, Y_train, X_val, Y_val = self._preprocess_df(df, validation_split)
 
@@ -65,5 +56,5 @@ class Seq2SeqForecaster(keras.models.Sequential):
         return history
 
     def plot_history(self, history_step=0):
-        plot_data = self.fit_history[-1 - history_step]
-        # plot it
+        # plot_data = self.fit_history[-1 - history_step]
+        raise NotImplementedError
